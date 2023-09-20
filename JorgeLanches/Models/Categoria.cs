db6b29614d7807a0a7b198122f75131794b0d186
Salvi Jorge <jorgessalvi@gmail.com>
2023-09-19 23:07:34 -0300

    Implementação do Data Annotations nos modelos do domínio para otimizar as colunas do banco.

--- JorgeLanches/Models/Categoria.cs
+++ JorgeLanches/Models/Categoria.cs
@@ -1,11 +1,21 @@
-﻿namespace JorgeLanches.Models
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JorgeLanches.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
 
+        [Required]
+        [MaxLength(80)]
         public string? Nome { get; set; }
 
+        [Required]
+        [MaxLength(300)]
         public string? ImagemUrl { get; set; }
 
         public ICollection<Produto>? produtos { get; set; }
--- JorgeLanches/Models/Produto.cs
+++ JorgeLanches/Models/Produto.cs
@@ -1,20 +1,31 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace JorgeLanches.Models
 {
+    [Table("Produtos")]
     public class Produto
     {
-
+        [Key]
         public int ProdutoId { get; set; }
 
+        [Required]
+        [StringLength(80)]
         public string? Nome { get; set; }
 
+        [Required]
+        [StringLength(80)]
         public string? Descricao { get; set; }
 
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
         public decimal Preco { get; set; }
 
+        [StringLength(300)]
         public string? ImagemUrl { get; set; }
 
+        
         public float Estoque { get; set; }
 
         public DateTime DataCadastro { get; set; }
