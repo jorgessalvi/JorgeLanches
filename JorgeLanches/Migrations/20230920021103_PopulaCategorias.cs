b3096d27ecb0ebf709f88232f2c83a7feaeee31e
Salvi Jorge <jorgessalvi@gmail.com>
2023-09-19 23:50:25 -0300

    As tabelas Categoria e Produto foram populadas utilizando migrations.

---
+++ JorgeLanches/Migrations/20230920021103_PopulaCategorias.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+
+#nullable disable
+
+namespace JorgeLanches.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.Sql("Insert into Categorias(Nome, ImagemUrl) Values('Bebidas', 'Drinks.jpg')");
+            migrationBuilder.Sql("Insert into Categorias(Nome, ImagemUrl) Values('Lanches', 'Foods.jpg')");
+            migrationBuilder.Sql("Insert into Categorias(Nome, ImagemUrl) Values('Sobremesas', 'Desserts.jpg')");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Categorias");
+        }
+    }
+}
