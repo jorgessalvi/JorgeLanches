da90659b35e2944f82d87bf4fd8298fbe417a318
Salvi Jorge <jorgessalvi@gmail.com>
2023-09-20 13:11:51 -0300

    Implementação dos 2 métodos Get no ProdutosController, para retornar todos os produtos ou pesquisar pelo Id.

---
+++ JorgeLanches/Controllers/ProdutosController.cs
@@ -0,0 +1,49 @@
+﻿using JorgeLanches.Context;
+using JorgeLanches.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JorgeLanches.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _Context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _Context = context;
+        }
+
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var listaProdutos = _Context.Produtos.ToList();
+
+            if(listaProdutos is null) { return NotFound(); }
+
+            return listaProdutos;
+
+        }
+
+
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _Context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+        
+            if(produto is null) { return NotFound(); }
+
+            return produto;
+            
+        }
+
+        
+
+
+    }
+}
