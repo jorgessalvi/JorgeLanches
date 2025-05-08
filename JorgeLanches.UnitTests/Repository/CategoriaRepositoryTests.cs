a04f361712918c201a390cf395ab3736aa260ba1
jorge salvi <jorgessalvi@gmail.com>
2025-05-08 11:06:29 -0300

    Implementação dos testes unitários dos Repositórios utilizando in memory db. Corrigido bug em CategoriaRepository que impedia um método de retornar null.

---
+++ JorgeLanches.UnitTests/Repository/CategoriaRepositoryTests.cs
@@ -0,0 +1,64 @@
+﻿using JorgeLanches.Context;
+using JorgeLanches.Models;
+using JorgeLanches.Pagination;
+using JorgeLanches.Repository;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace JorgeLanches.UnitTests;
+
+public class CategoriaRepositoryTests
+{
+    private AppDbContext _dbContext;
+    private CategoriaRepository _categoriaRepository;
+
+    [SetUp]
+    public async Task Setup()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>().UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+        _dbContext = new AppDbContext(options);
+        _dbContext.Database.EnsureDeleted();
+        _dbContext.Database.EnsureCreated();
+
+        _categoriaRepository = new CategoriaRepository(_dbContext);
+
+        _categoriaRepository.Create(CreateNewCategoria(1));
+        _categoriaRepository.Create(CreateNewCategoria(2));
+        await _categoriaRepository.CommitAsync();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _dbContext.Dispose();
+    }
+
+    [Test]
+    public async Task GetCategoriaPorNomeAsync_NameNotFound_ReturnNull()
+    {
+        var filtro = new CategoriaFilterParameters { Identificação = "a" };
+        var categorias = await _categoriaRepository.GetCategoriaPorNomeAsync(filtro);
+
+        Assert.IsNull(categorias);
+    }
+
+    [Test]
+    public async Task GetCategoriaPorNomeAsync_WhenCalled_ReturnCategoriasFilteredByName()
+    {
+        var filtro = new CategoriaFilterParameters { Identificação = "1" };
+        var categorias = await _categoriaRepository.GetCategoriaPorNomeAsync(filtro);
+
+        Assert.That(categorias.First().Nome, Does.Contain("1"));
+        Assert.That(categorias.Count(), Is.EqualTo(1));
+    }
+
+    private static Categoria CreateNewCategoria(int param)
+    {
+        return new Categoria
+        {
+            Id = param,
+            ImagemUrl = param.ToString(),
+            Nome = param.ToString()
+        };
+    }
+}
---
+++ JorgeLanches.UnitTests/Repository/ProdutoRepositoryTests.cs
@@ -0,0 +1,70 @@
+﻿using JorgeLanches.Context;
+using JorgeLanches.Models;
+using JorgeLanches.Pagination;
+using JorgeLanches.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace JorgeLanches.UnitTests;
+
+public class ProdutoRepositoryTests
+{
+    private AppDbContext _dbContext;
+    private ProdutoRepository _produtoRepository;
+
+    [SetUp]
+    public async Task Setup()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>().UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+        _dbContext = new AppDbContext(options);
+        _dbContext.Database.EnsureDeleted();
+        _dbContext.Database.EnsureCreated();
+
+        _produtoRepository = new ProdutoRepository(_dbContext);
+
+        _produtoRepository.Create(GetNewProduto(1));
+        _produtoRepository.Create(GetNewProduto(2));
+        await _produtoRepository.CommitAsync();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _dbContext.Dispose();
+    }
+
+    [Test]
+    public async Task GetProdutosPorCategoriaAsync_WhenCalled_ShouldReturnProductsOnlyFromChosenCategoryId()
+    {
+        var produtos = await _produtoRepository.GetProdutosPorCategoriaAsync(2);
+
+        Assert.That(produtos.Where(p => p.CategoriaId == 2), Is.Not.Empty);
+        Assert.That(produtos.Where(p => p.CategoriaId == 1), Is.Empty);
+    }
+
+    [Test]
+    [TestCase("maior", 1, 2)]
+    [TestCase("igual", 1, 1)]
+    [TestCase("menor", 2, 1)]
+    public async Task GetProdutosFiltroPrecoAsync_WhenCalled_ReturnExpectedResult(string criterio, int preco, int expectedResult)
+    {
+        var filters = new ProdutoFilterParameters { CriterioFiltroPreco = criterio, Preco = preco };
+        var produtos = await _produtoRepository.GetProdutosFiltroPrecoAsync(filters);
+
+        Assert.That(produtos.Where(p => p.Preco == expectedResult), Is.Not.Empty);
+        Assert.That(produtos.Where(p => p.Preco != expectedResult), Is.Empty);
+    }
+
+    private static Produto GetNewProduto(int param)
+    {
+        return new Produto
+        {
+            Id = param,
+            Nome = param.ToString(),
+            Descricao = param.ToString(),
+            Preco = param,
+            CategoriaId = param,
+            ImagemUrl = param.ToString(),
+            QtdEstoque = param
+        };
+    }
+}
